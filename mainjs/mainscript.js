d1cd64fc223a989375d4915a77a2e1672c4790bb
Seftort <ljy19913606261@outlook.com>
2024-03-09 22:30:23 +0800

    2024/03/09

--- mainjs/mainscript.js
+++ mainjs/mainscript.js
@@ -93,7 +93,7 @@ container.addEventListener("mouseleave", () => {
 });
 
 
-  
+
 
 
 
@@ -108,4 +108,66 @@ window.addEventListener('scroll', function() {
 	} else {
 		topNav.style.top = '-60px';
 	}
+});
+
+
+
+
+
+function search() {
+	const searchInput = document.getElementById('searchInput').value.toLowerCase();
+	const resultsContainer = document.getElementById('results');
+	resultsContainer.innerHTML = '';
+
+	if (searchInput.trim() === '') {
+		return; // If the input is empty, do not display any results
+	}
+
+	fetch('wenzhang.json') // Replace 'your_external_data.json' with the actual path to your JSON file
+		.then(response => response.json())
+		.then(data => {
+			for (const key in data) {
+				const name = data[key].name.toLowerCase();
+
+				if (name.includes(searchInput)) {
+					const listItem = document.createElement('li');
+					listItem.className = 'result-item';
+					listItem.textContent = name;
+					listItem.setAttribute('data-url', data[key].url);
+					listItem.addEventListener('click', redirectToURL);
+					resultsContainer.appendChild(listItem);
+				}
+			}
+		})
+		.catch(error => console.error('Error fetching data:', error));
+}
+
+function redirectToURL(event) {
+	const selectedUrl = event.currentTarget.getAttribute('data-url');
+	window.location.href = selectedUrl;
+}
+
+
+
+
+
+
+$(document).ready(function() {
+	// 点击搜索图标时显示搜索框
+	$('.search').on('click', function() {
+		$(".search-c")[0].style.display = 'block';
+	});
+
+	// 点击搜索框背景时隐藏搜索框
+	$('.search-c').on('click', function(e) {
+		if (e.target === this) {
+			$(".search-c")[0].style.display = 'none';
+		}
+	});
+
+	// 阻止搜索框内部点击事件冒泡到父级，防止触发上面的隐藏事件
+	$('.search-z').on('click', function(e) {
+		e.stopPropagation();
+	});
+
 });
